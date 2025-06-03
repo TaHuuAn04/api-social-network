84fddd922bc13c8e5a894fb215597d9d8a8a92ec
TaHuuAn04 <an.taduychinh@hcmut.edu.vn>
2025-06-03 14:32:31 +0700

    first commit

---
+++ src/main/java/com/example/apisocialnetwork/ApiSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSocialNetworkApplication.class, args);
+    }
+
+}
